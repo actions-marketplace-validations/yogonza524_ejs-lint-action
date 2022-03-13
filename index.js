a6b48e2de82f1476ae68e9c2c83e7e709ee6545d
Gonza <gonzalo.mendoza@wenance.com>
2022-03-13 16:03:15 -0300

    Base Project

---
+++ index.js
@@ -0,0 +1,40 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const ejsLint = require('ejs-lint');
+const fs = require('fs');
+regex = new RegExp('[*.ejs]$');
+
+try {
+    const folder = core.getInput('folder');
+
+    // const folder = '.';
+
+    console.log(`Folder: ${folder}`);
+    const files = fs.readdirSync(folder);
+
+    console.log(`files: ${files}`)
+
+    const filesArray = files
+        .toString()
+        .split(",")
+        .map(function(file, index) {
+            const ejs = file.split('.').filter(filePart => filePart.length > 1);
+            if (ejs && ejs[1] == 'ejs') {
+                return file;
+            }
+        })
+        .filter(item => item !== undefined);
+
+    filesArray.forEach(file => {
+        console.log(`EJS File: ${file}`);
+
+        var text = fs.readFileSync(file).toString('utf-8');
+
+        const validation = ejsLint(`${text}`);
+
+        console.log(`Validation: ${validation}`);
+
+    });
+} catch(error) {
+    core.setFailed(error.message);
+}
